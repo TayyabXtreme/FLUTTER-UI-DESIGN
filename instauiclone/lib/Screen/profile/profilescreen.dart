2002e3c68b898705a3c5322695b29dc6127611f4
Muhammad Tayyab <tayyabxtrem@gmail.com>
2024-12-27 17:42:18 +0500

    profile page

--- instauiclone/lib/Screen/profile/profilescreen.dart
+++ instauiclone/lib/Screen/profile/profilescreen.dart
@@ -6,7 +6,29 @@ class Profilescreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Profile')),
+      appBar: AppBar(
+        title: Text(
+          'Anees@w',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+        ),
+        centerTitle: true,
+        actions: [Icon(Icons.list_outlined)],
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+              height: 100,
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      'https://th.bing.com/th/id/R.d064a09d90d5c177c4813b582941c189?rik=oRNqtsfDamTI7Q&pid=ImgRaw&r=0',
+                    ),
+                  )
+                ],
+              ))
+        ],
+      ),
     );
   }
 }
