f8cb8d4c5f09eecb2e854eb7a788c0b1476aeb37
Muhammad Tayyab <tayyabxtrem@gmail.com>
2024-12-29 22:33:40 +0500

    cart screen

--- blinkituiclone/lib/repository/widgets/Uihelper.dart
+++ blinkituiclone/lib/repository/widgets/Uihelper.dart
@@ -20,4 +20,40 @@ class UiHelper {
           color: color),
     );
   }
+
+  static CutomTextField({required TextEditingController controller}) {
+    return Container(
+      height: 37,
+      width: 326,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+          border: Border.all(color: Color(0xffC5C5C5))),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+            hintText: "Search 'ice-cream",
+            prefixIcon: UiHelper.CustomImage(img: 'search.png'),
+            suffixIcon: UiHelper.CustomImage(img: 'mic.png'),
+            border: InputBorder.none),
+      ),
+    );
+  }
+
+  static CustomButton(VoidCallback callback) {
+    return Container(
+      height: 18,
+      width: 30,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(4),
+          border: Border.all(color: Color(0xff27af34))),
+      child: Center(
+        child: Text(
+          'Add',
+          style: TextStyle(fontSize: 8, color: Color(0xff27af34)),
+        ),
+      ),
+    );
+  }
 }
