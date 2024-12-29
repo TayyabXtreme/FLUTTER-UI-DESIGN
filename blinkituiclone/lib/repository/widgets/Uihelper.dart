cafde145e5aeea5993def253a01e5856e50531ad
Muhammad Tayyab <tayyabxtrem@gmail.com>
2024-12-29 20:13:14 +0500

    login screen

--- blinkituiclone/lib/repository/widgets/Uihelper.dart
+++ blinkituiclone/lib/repository/widgets/Uihelper.dart
@@ -4,4 +4,20 @@ class UiHelper {
   static CustomImage({required String img}) {
     return Image.asset("assets/images/$img");
   }
+
+  static CutomText(
+      {required String text,
+      required Color color,
+      required FontWeight fontWeight,
+      String? fontFamily,
+      required double fontSize}) {
+    return Text(
+      text,
+      style: TextStyle(
+          fontSize: fontSize,
+          fontFamily: fontFamily ?? "regular",
+          fontWeight: fontWeight,
+          color: color),
+    );
+  }
 }
